f532f4d11b743b52e80329dc26f8b15507fbeb0c
Facu Fuensalida <ffuensalida7@gmail.com>
2026-05-04 21:59:25 -0500

    Update WhatsApp orders number to confirmed 381 366-4395

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,1 +1,1 @@
-export const whatsappURL = `https://wa.me/5493815033459?text=${encodeURIComponent('Hola! Quería hacer una consulta.')}`;
+export const whatsappURL = `https://wa.me/5493813664395?text=${encodeURIComponent('Hola! Quería hacer una consulta.')}`;
